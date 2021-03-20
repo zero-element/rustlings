c7c078f767ce7703a1008ae2d1cd5d2e921e9578
zer0 <2388597213@qq.com>
2021-03-21 00:21:17 +0800

    basic

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,7 +7,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,7 +17,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(&vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
